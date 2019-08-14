2de71e40a09c527afd76fe3888e9e9e42fa25581
Kaittrakul jaroenpong 6206021620159 IT1RA <45314100+1F659162@users.noreply.github.com>
2019-08-14 18:44:11 +0700

    Update Mid_62_02.cpp

--- Mid_62_02.cpp
+++ Mid_62_02.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main()
 {
 	string num ;
-	int lnum , total = 0;
+	int lnum , last , count = 1 , total = 0;
 	char show ='s' ;
 
 	cout << "Enter Binary : ";
 	cin >> num;
 	lnum = num.length();
+	last = lnum-1;
 
 	if(lnum != 5)
 	{
@@ -20,16 +21,13 @@ int main()
 		for(int i = 0 ; i < lnum ; i++)
 		{
 			if (num.at(i) != '0' && num.at(i) != '1')
-				{
-					cout << num.at(i) << " <-- this isn't binary." << endl;
-					show = 'n';
-				}
-			else cout << num.at(i) << endl;
-			if(num.at(i) == '1' && i == 0) total += 16;
-			if(num.at(i) == '1' && i == 1) total += 8;
-			if(num.at(i) == '1' && i == 2) total += 4;
-			if(num.at(i) == '1' && i == 3) total += 2;
-			if(num.at(i) == '1' && i == 4) total += 1;
+			{
+				cout << num.at(i) << " <-- this isn't binary." << endl;
+				show = 'n';
+			}else cout << num.at(i) << endl;
+
+			if(num.at(last) == '1')total += count ;
+			count *= 2 ;
 		}
 	}
 	if(show == 's')cout << "Decimal number : " << total << endl;
